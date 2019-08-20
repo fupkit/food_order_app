75c9a6b6bef9babcd350ce40eadc9fdfbda1dab2
Fupk <fupkit@hotmail.com>
2019-08-20 21:07:20 +0800

    new data

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ var app = express();
 
 app.get("/mobile_app/food_order_app/get_menu", (req, res, next) => {
     res.json(JSON.parse(fs.readFileSync("data.json")));
-   });
+});
 
+app.use('/images', express.static(__dirname + '/images'));
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
